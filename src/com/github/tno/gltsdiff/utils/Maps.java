2f772ae56d52ead646096498f5f740bf3c1477d0
wytseoortwijn <wytseoortwijn@gmail.com>
2022-12-14 20:20:15 +0100

    Apply suggestions from code review
    Co-authored-by: Dennis Hendriks <dhendriks@users.noreply.github.com>

--- src/com/github/tno/gltsdiff/utils/Maps.java
+++ src/com/github/tno/gltsdiff/utils/Maps.java
@@ -6,13 +6,13 @@ import java.util.stream.Collector;
 
 public class Maps {
     /**
-     * A binary operator to merge mappings that can be used by {@link Collector collectors}, which always returns a
+     * A binary operator to merge values that can be used by {@link Collector collectors}, which always returns an
      * {@link IllegalStateException}.
      * 
-     * @param <T> The type of elements to merge.
+     * @param <T> The type of values to merge.
      * @return A binary operator that always throws a {@link IllegalStateException} when applied.
      */
     public static <T> BinaryOperator<T> throwingMerger() {
-        return (u, v) -> { throw new IllegalStateException(String.format("Duplicate key %s", u)); };
+        return (u, v) -> { throw new IllegalStateException(String.format("Attempted to merge values %s and %s", u, v)); };
     }
 }
