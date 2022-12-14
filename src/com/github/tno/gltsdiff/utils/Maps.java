8af99ce3dbb84c89cca8cb5300d15acaf894d3bd
woortwij <wytse.oortwijn@asml.com>
2022-12-14 17:55:22 +0100

    some changes to javadoc after self review

--- src/com/github/tno/gltsdiff/utils/Maps.java
+++ src/com/github/tno/gltsdiff/utils/Maps.java
@@ -2,13 +2,14 @@
 package com.github.tno.gltsdiff.utils;
 
 import java.util.function.BinaryOperator;
+import java.util.stream.Collector;
 
 public class Maps {
     /**
-     * A merger for values of a map that can be used by {@link Collector collectors}, which always returns a
+     * A binary operator to merge mappings that can be used by {@link Collector collectors}, which always returns a
      * {@link IllegalStateException}.
      * 
-     * @param <T> The type of values to merge.
+     * @param <T> The type of elements to merge.
      * @return A binary operator that always throws a {@link IllegalStateException} when applied.
      */
     public static <T> BinaryOperator<T> throwingMerger() {
