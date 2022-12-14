01cd7365e8df06c3ce742891343c24b5f91e70df
woortwij <wytse.oortwijn@asml.com>
2022-12-14 17:48:12 +0100

    threw away the custom toMap implementation

--- src/com/github/tno/gltsdiff/utils/PredictableCollectors.java
+++ src/com/github/tno/gltsdiff/utils/PredictableCollectors.java
@@ -1,28 +1,17 @@
 
 package com.github.tno.gltsdiff.utils;
 
-import java.util.LinkedHashMap;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+import java.util.function.BinaryOperator;
 
 public class PredictableCollectors {
     /**
-     * A version of {@link Collectors#toMap} that writes to a {@link LinkedHashMap} instead of an {@link HashMap},
-     * thereby letting the returned map have predictable iteration order.
+     * A merger for values of a map that can be used by {@link Collector collectors}, which always returns a
+     * {@link IllegalStateException}.
      * 
-     * @param <T> the type of the input elements.
-     * @param <K> the output type of the key mapping function.
-     * @param <U> the output type of the value mapping function.
-     * @param keyMapper a mapping function to produce keys.
-     * @param valueMapper a mapping function to produce values.
-     * @return A collector as given by {@link Collectors#toMap}, which collects to a {@link LinkedHashMap}.
+     * @param <T> The type of values to merge.
+     * @return A binary operator that always throws a {@link IllegalStateException} when applied.
      */
-    public static <T, K, U> Collector<T, ?, LinkedHashMap<K, U>> toMap(Function<? super T, ? extends K> keyMapper,
-            Function<? super T, ? extends U> valueMapper)
-    {
-        return Collectors.toMap(keyMapper, valueMapper,
-                (l, r) ->
-                { throw new IllegalArgumentException("Expected no duplicate values."); }, LinkedHashMap::new);
+    public static <T> BinaryOperator<T> throwingMerger() {
+        return (u, v) -> { throw new IllegalStateException(String.format("Duplicate key %s", u)); };
     }
 }
