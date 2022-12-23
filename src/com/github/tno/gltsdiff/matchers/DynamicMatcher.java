72a348fb81912e34cfd47aa3e41bce694dc87f60
woortwij <wytse.oortwijn@asml.com>
2022-12-23 16:44:49 +0100

    letting DynamicMatcher work on GLTSs

--- src/com/github/tno/gltsdiff/matchers/DynamicMatcher.java
+++ src/com/github/tno/gltsdiff/matchers/DynamicMatcher.java
@@ -13,26 +13,26 @@ package com.github.tno.gltsdiff.matchers;
 import java.util.Map;
 import java.util.function.BiFunction;
 
-import com.github.tno.gltsdiff.glts.LTS;
+import com.github.tno.gltsdiff.glts.GLTS;
 import com.github.tno.gltsdiff.glts.State;
-import com.github.tno.gltsdiff.matchers.scorers.DynamicLTSScorer;
+import com.github.tno.gltsdiff.matchers.scorers.DynamicGLTSScorer;
 import com.github.tno.gltsdiff.matchers.scorers.SimilarityScorer;
 import com.github.tno.gltsdiff.operators.combiners.Combiner;
 
 /**
  * Contains functionality for computing state matchings that makes a trade-off between computational intensity and the
- * quality of the computed matchings. Different matching algorithms can be used for different input LTSs, e.g. based on
- * their sizes (numbers of states) from "heavyweight" (for smaller LTSs) to "lightweight" (for larger LTSs).
+ * quality of the computed matchings. Different matching algorithms can be used for different input GLTSs, e.g. based on
+ * their sizes (numbers of states) from "heavyweight" (for smaller GLTSs) to "lightweight" (for larger GLTSs).
  *
  * @param <S> The type of state properties.
  * @param <T> The type of transition properties.
- * @param <U> The type of LTSs.
+ * @param <U> The type of GLTSs.
  */
-public class DynamicMatcher<S, T, U extends LTS<S, T>> implements Matcher<S, T, U> {
-    /** The left-hand-side LTS. */
+public class DynamicMatcher<S, T, U extends GLTS<S, T>> implements Matcher<S, T, U> {
+    /** The left-hand-side GLTS. */
     private final U lhs;
 
-    /** The right-hand-side LTS. */
+    /** The right-hand-side GLTS. */
     private final U rhs;
 
     /** The combiner for state properties. */
@@ -41,14 +41,16 @@ public class DynamicMatcher<S, T, U extends LTS<S, T>> implements Matcher<S, T, 
     /** The combiner for transition properties. */
     protected final Combiner<T> transitionPropertyCombiner;
 
-    /** The matching algorithm creator. Given the input LTSs and appropriate combiners, creates a suitable algorithm. */
+    /**
+     * The matching algorithm creator. Given the input GLTSs and appropriate combiners, creates a suitable algorithm.
+     */
     private final BiFunction<U, U, BiFunction<Combiner<S>, Combiner<T>, Matcher<S, T, U>>> matchingAlgorithmCreator;
 
     /**
      * Instantiates a new dynamic matching algorithm, that uses a default configuration of matching algorithms.
      * 
-     * @param lhs The left-hand-side LTS.
-     * @param rhs The right-hand-side LTS.
+     * @param lhs The left-hand-side GLTS.
+     * @param rhs The right-hand-side GLTS.
      * @param statePropertyCombiner The combiner for state properties.
      * @param transitionPropertyCombiner The combiner for transition properties.
      */
@@ -60,11 +62,11 @@ public class DynamicMatcher<S, T, U extends LTS<S, T>> implements Matcher<S, T, 
     /**
      * Instantiates a new dynamic matching algorithm.
      * 
-     * @param lhs The left-hand-side LTS.
-     * @param rhs The right-hand-side LTS.
+     * @param lhs The left-hand-side GLTS.
+     * @param rhs The right-hand-side GLTS.
      * @param statePropertyCombiner The combiner for state properties.
      * @param transitionPropertyCombiner The combiner for transition properties.
-     * @param matchingAlgorithmCreator The matching algorithm creator. Given the input LTSs and appropriate combiners,
+     * @param matchingAlgorithmCreator The matching algorithm creator. Given the input GLTSs and appropriate combiners,
      *     creates a suitable algorithm.
      */
     public DynamicMatcher(U lhs, U rhs, Combiner<S> statePropertyCombiner, Combiner<T> transitionPropertyCombiner,
@@ -87,14 +89,14 @@ public class DynamicMatcher<S, T, U extends LTS<S, T>> implements Matcher<S, T, 
         return rhs;
     }
 
-    private static final <S, T, U extends LTS<S, T>> Matcher<S, T, U> defaultMatchingAlgorithmCreator(U lhs, U rhs,
+    private static final <S, T, U extends GLTS<S, T>> Matcher<S, T, U> defaultMatchingAlgorithmCreator(U lhs, U rhs,
             Combiner<S> statePropertyCombiner, Combiner<T> transitionPropertyCombiner)
     {
-        SimilarityScorer<S, T, U> scorer = new DynamicLTSScorer<>(lhs, rhs, statePropertyCombiner,
+        SimilarityScorer<S, T, U> scorer = new DynamicGLTSScorer<>(lhs, rhs, statePropertyCombiner,
                 transitionPropertyCombiner);
 
         if (lhs.size() > 45 || rhs.size() > 45) {
-            return new WalkinshawLTSMatcher<>(lhs, rhs, scorer, statePropertyCombiner, transitionPropertyCombiner);
+            return new WalkinshawGLTSMatcher<>(lhs, rhs, scorer, statePropertyCombiner, transitionPropertyCombiner);
         } else {
             return new KuhnMunkresMatcher<>(lhs, rhs, scorer, statePropertyCombiner);
         }
