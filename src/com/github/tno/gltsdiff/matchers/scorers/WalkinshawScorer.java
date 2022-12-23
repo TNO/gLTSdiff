5e9958f35c8e272c924aee74de6b7d5013e4858d
woortwij <wytse.oortwijn@asml.com>
2022-12-23 10:48:35 +0100

    formatting

--- src/com/github/tno/gltsdiff/matchers/scorers/WalkinshawScorer.java
+++ src/com/github/tno/gltsdiff/matchers/scorers/WalkinshawScorer.java
@@ -49,9 +49,9 @@ public abstract class WalkinshawScorer<S, T, U extends GLTS<S, T>> implements Si
      * the final similarity scores.
      * <p>
      * A ratio of 0 would mean that only local similarity scores are used. Note that, if one is only interested in local
-     * similarity, {@link WalkinshawLocalGLTSScorer} should be used instead, which gives the same result but is much cheaper
-     * in terms of computation. A ratio of 1 would mean that far-away state similarities contribute equally much as
-     * local ones.
+     * similarity, {@link WalkinshawLocalGLTSScorer} should be used instead, which gives the same result but is much
+     * cheaper in terms of computation. A ratio of 1 would mean that far-away state similarities contribute equally much
+     * as local ones.
      * </p>
      * <p>
      * This factor can be tweaked a bit if the comparison results come out unsatisfactory.
