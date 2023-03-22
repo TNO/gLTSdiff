32426e4238b8cc3c0a5d3e39a95e51d80c2e12bc
Dennis Hendriks <dhendriks@users.noreply.github.com>
2023-03-22 16:44:31 +0100

    #12 WalkinshawLTSMatcherTest improved JavaDoc.

--- src-test/com/github/tno/gltsdiff/matchers/WalkinshawLTSMatcherTest.java
+++ src-test/com/github/tno/gltsdiff/matchers/WalkinshawLTSMatcherTest.java
@@ -16,7 +16,7 @@ import com.github.tno.gltsdiff.matchers.scorers.SimilarityScorer;
 import com.github.tno.gltsdiff.operators.combiners.AutomatonStatePropertyCombiner;
 import com.github.tno.gltsdiff.operators.combiners.EqualityCombiner;
 
-/** Tests for the {@link WalkinshawLTSMatcher}. */
+/** {@link WalkinshawLTSMatcher} tests. */
 public class WalkinshawLTSMatcherTest extends MatcherTest {
     @Override
     public <T> Matcher<AutomatonStateProperty, T, SimpleAutomaton<T>>
