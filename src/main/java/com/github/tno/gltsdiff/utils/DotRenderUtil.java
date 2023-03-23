517879039f9ec687d990c38f8fed2476f48625a0
Dennis Hendriks <dhendriks@users.noreply.github.com>
2023-03-23 15:23:25 +0100

    #101 DotRenderUtil: various improvements.
    - RuntimeException instead of InterruptedException.
    - Allow rendering with custom or automatic output path.

--- src/main/java/com/github/tno/gltsdiff/utils/DotRenderUtil.java
+++ src/main/java/com/github/tno/gltsdiff/utils/DotRenderUtil.java
@@ -12,6 +12,8 @@ package com.github.tno.gltsdiff.utils;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.google.common.base.Preconditions;
 
@@ -31,12 +33,29 @@ public class DotRenderUtil {
      * </p>
      *
      * @param dotFilePath The path to the DOT file to render.
+     * @note The path for the resulting SVG file is the DOT file path with '.svg' added to it.
      * @throws IOException In case of an I/O error.
-     * @throws InterruptedException If the current thread is interrupted by another thread while it is waiting for the
-     *     DOT process to terminate.
      * @throws IllegalStateException If the DOT executable did not successfully terminate.
      */
-    public static void renderDot(Path dotFilePath) throws IOException, InterruptedException {
+    public static void renderDot(Path dotFilePath) throws IOException {
+        renderDot(dotFilePath, null);
+    }
+
+    /**
+     * Renders a DOT file using 'dot' (part of GraphViz).
+     *
+     * <p>
+     * If the 'DOT_PATH' environment variable is set, the DOT executable path is taken from that variable. Otherwise
+     * "dot" is used as executable, and 'dot' must on the 'PATH'.
+     * </p>
+     *
+     * @param dotFilePath The path to the DOT file to render.
+     * @param svgFilePath The path to the SVG file to produce. May be {@code null} to use the DOT file path with '.svg'
+     *     added to it.
+     * @throws IOException In case of an I/O error.
+     * @throws IllegalStateException If the DOT executable did not successfully terminate.
+     */
+    public static void renderDot(Path dotFilePath, Path svgFilePath) throws IOException {
         // Get DOT executable path.
         String dotExecPath = System.getenv("DOT_PATH");
         if (dotExecPath == null || dotExecPath.trim().isEmpty()) {
@@ -45,12 +64,27 @@ public class DotRenderUtil {
 
         // Create the process.
         ProcessBuilder builder = new ProcessBuilder();
-        builder.command(dotExecPath, "-Tsvg", "-O", dotFilePath.toString());
+        List<String> command = new ArrayList<>();
+        command.add(dotExecPath);
+        command.add("-Tsvg");
+        if (svgFilePath == null) {
+            command.add("-O");
+        } else {
+            command.add("-o");
+            command.add(svgFilePath.toString());
+        }
+        command.add(dotFilePath.toString());
+        builder.command(command);
         builder.inheritIO();
 
         // Start the process and wait for it to terminate.
         Process process = builder.start();
-        int exitCode = process.waitFor();
+        int exitCode;
+        try {
+            exitCode = process.waitFor();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted waiting for DOT to finish.", e);
+        }
 
         // Check that DOT terminated successfully.
         Preconditions.checkState(exitCode == 0,
