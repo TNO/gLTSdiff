388b0fe439cf7ad780a2b623d7f344c851df5ece
Dennis Hendriks <dhendriks@users.noreply.github.com>
2023-03-28 18:19:26 +0200

    #14 Add GLTSStructureComparatorBuilder for concrete GLTSs.

---
+++ src/main/java/com/github/tno/gltsdiff/builders/GLTSStructureComparatorBuilder.java
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 Contributors to the GitHub community
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package com.github.tno.gltsdiff.builders;
+
+import com.github.tno.gltsdiff.glts.GLTS;
+
+/**
+ * {@link StructureComparatorBuilder Structure comparator builder} to more easily configure the various settings for
+ * comparing, merging and writing {@link GLTS GLTSs}.
+ *
+ * @param <S> The type of state properties.
+ * @param <T> The type of transition properties.
+ */
+public class GLTSStructureComparatorBuilder<S, T> extends StructureComparatorBuilder<S, T, GLTS<S, T>> {
+    /** Instantiates a new GLTS structure comparator builder. */
+    public GLTSStructureComparatorBuilder() {
+        super();
+        setInstantiator(() -> new GLTS<>());
+    }
+}
