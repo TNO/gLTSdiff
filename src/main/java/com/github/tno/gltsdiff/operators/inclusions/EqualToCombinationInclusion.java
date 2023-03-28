f375382ce81a37a19227fe1eecee178ec23629e4
Dennis Hendriks <dhendriks@users.noreply.github.com>
2023-03-28 13:58:18 +0200

    #14 Define Inclusion operator and EqualToCombinationInclusion.

---
+++ src/main/java/com/github/tno/gltsdiff/operators/inclusions/EqualToCombinationInclusion.java
@@ -0,0 +1,56 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 Contributors to the GitHub community
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package com.github.tno.gltsdiff.operators.inclusions;
+
+import java.util.Objects;
+
+import com.github.tno.gltsdiff.operators.combiners.Combiner;
+import com.google.common.base.Preconditions;
+
+/**
+ * An inclusion operator that checks whether the combination of two properties is {@link Object#equals equal} to both
+ * those properties.
+ *
+ * <p>
+ * Assumptions:
+ * </p>
+ * <ul>
+ * <li>Inclusion must only be checked for combinable properties.</li>
+ * <li>The combination of the properties must either be equal to both properties, or be unequal to both properties.</li>
+ * </ul>
+ *
+ * @param <T> The type of properties for which to check inclusion.
+ */
+public class EqualToCombinationInclusion<T> implements Inclusion<T> {
+    /** The property combiner to use. */
+    private final Combiner<T> combiner;
+
+    /**
+     * Instantiates a new equal to combination inclusion operator.
+     *
+     * @param combiner The property combiner to use.
+     */
+    public EqualToCombinationInclusion(Combiner<T> combiner) {
+        this.combiner = combiner;
+    }
+
+    @Override
+    public boolean isIncludedIn(T property1, T property2) {
+        Preconditions.checkArgument(combiner.areCombinable(property1, property2),
+                "Expected properties to be combinable.");
+        T combination = combiner.combine(property1, property2);
+        boolean equal1 = Objects.equals(property1, combination);
+        boolean equal2 = Objects.equals(property2, combination);
+        Preconditions.checkArgument(equal1 == equal2,
+                "Expected combination to be either equal to both properties or unequal to both properties.");
+        return equal1;
+    }
+}
---
+++ src/main/java/com/github/tno/gltsdiff/operators/inclusions/Inclusion.java
@@ -0,0 +1,59 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 Contributors to the GitHub community
+//
+// This program and the accompanying materials are made available
+// under the terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT
+//
+// SPDX-License-Identifier: MIT
+//////////////////////////////////////////////////////////////////////////////
+
+package com.github.tno.gltsdiff.operators.inclusions;
+
+import java.util.Objects;
+import java.util.Set;
+
+import com.github.tno.gltsdiff.glts.lts.automaton.diff.DiffKind;
+
+/**
+ * An operator for determining property inclusion.
+ *
+ * <p>
+ * Inclusion describes whether one property is included in another ({@link #isIncludedIn}). For example:
+ * </p>
+ * <ul>
+ * <li>When comparing {@link Set}s of integer numbers, a set {1, 2, 4} is included in set {1, 2, 3, 4, 5}, as all the
+ * integer numbers that are in the first set are also in the second set.</li>
+ * <li>When comparing {@link DiffKind difference kinds}, if something is both added (in one input model) and removed (in
+ * the other input model), then it is really unchanged. The unchanged difference kind thus is a combination of being
+ * added and removed. Hence, {@link DiffKind#UNCHANGED} includes {@link DiffKind#ADDED} and
+ * {@link DiffKind#REMOVED}.</li>
+ * </ul>
+ *
+ * @param <T> The type of properties, for which to determine inclusion.
+ */
+public interface Inclusion<T> {
+    /**
+     * Binary operator for checking inclusion between two specified property.
+     *
+     * <p>
+     * This operator must be:
+     * </p>
+     * <ul>
+     * <li><u>reflexive</u>: Every property is included in itself, meaning {@link #isIncludedIn
+     * isIncludedIn}{@code (x, x)} always returns {@code true}.</li>
+     * <li><u>asymmetric</u>: If {@link #isIncludedIn isIncludedIn}{@code (x, y)} returns {@code true} and
+     * {@link Objects#equals}{@code (x, y)}, then {@link #isIncludedIn isIncludedIn}{@code (y, x)} returns
+     * {@code false}.</li>
+     * <li><u>transitive</u>: If {@link #isIncludedIn isIncludedIn}{@code (x, y)} returns {@code true} and
+     * {@link #isIncludedIn isIncludedIn}{@code (y, z)} returns {@code true}, then also {@link #isIncludedIn
+     * isIncludedIn}{@code (x, z)} returns {@code true}.</li>
+     * </ul>
+     *
+     * @param property1 The first property, to check whether it is included in the second property. Must not be
+     *     {@code null}.
+     * @param property2 The second property, to check whether it includes the first property. Must not be {@code null}.
+     * @return {@code true} if the first property is included in the second property, {@code false} otherwise.
+     */
+    public boolean isIncludedIn(T property1, T property2);
+}
