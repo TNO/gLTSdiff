2659ca464dd51e127a47586070a811968d672a26
Dennis Hendriks <dhendriks@users.noreply.github.com>
2023-03-28 16:39:31 +0200

    #14 Separate FixedPointRewriter from SequenceRewriter.
    - Also some JavaDoc improvements.

--- src/main/java/com/github/tno/gltsdiff/rewriters/FixedPointRewriter.java
+++ src/main/java/com/github/tno/gltsdiff/rewriters/FixedPointRewriter.java
@@ -10,38 +10,37 @@
 
 package com.github.tno.gltsdiff.rewriters;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import com.github.tno.gltsdiff.glts.GLTS;
 
 /**
- * A rewriter that applies multiple rewriters in sequence.
+ * A rewriter that repeatedly applies a rewriter until it no longer changes the GLTS.
  *
  * @param <S> The type of state properties.
  * @param <T> The type of transition properties.
  * @param <U> The type of GLTSs to rewrite.
  */
-public class SequenceRewriter<S, T, U extends GLTS<S, T>> implements Rewriter<S, T, U> {
-    /** The rewriters to apply in sequence. */
-    private final List<Rewriter<S, T, U>> rewriters;
+public class FixedPointRewriter<S, T, U extends GLTS<S, T>> implements Rewriter<S, T, U> {
+    /** The rewriter to repeatedly apply. */
+    private final Rewriter<S, T, U> rewriter;
 
     /**
-     * Instantiates a sequence rewriter.
+     * Instantiates a fixed point rewriter.
      *
-     * @param rewriters The rewriters to apply in sequence.
+     * @param rewriter The rewriter to repeatedly apply.
      */
-    public SequenceRewriter(Collection<Rewriter<S, T, U>> rewriters) {
-        this.rewriters = new ArrayList<>(rewriters);
+    public FixedPointRewriter(Rewriter<S, T, U> rewriter) {
+        this.rewriter = rewriter;
     }
 
     @Override
     public boolean rewrite(U glts) {
-        boolean changed = false;
-        for (Rewriter<S, T, U> rewriter: rewriters) {
-            changed |= rewriter.rewrite(glts);
+        int rounds = 0;
+        while (true) {
+            rounds++;
+            boolean changed = rewriter.rewrite(glts);
+            if (!changed) {
+                return rounds > 1;
+            }
         }
-        return changed;
     }
 }
